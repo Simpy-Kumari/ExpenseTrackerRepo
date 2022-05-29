3374a047b12d10708aae8644c7544d458f6fb52b
Sippu <sippu@Simpys-MacBook-Air.local>
2022-05-29 16:31:45 +0530

    updated my daily expenses

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
 
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./Components/Expenses/Expenses.js";
 import NewExpenses from "./Components/NewExpenses/NewExpenses.js";
 
+let Expense_data = [
+    {
+        id: "en1",
+        title: "School Fee",
+        amount: 600,
+        date: new Date(2022, 4, 12)
+
+    },
+
+    {
+        id: "en2",
+        title: "House Rent",
+        amount: 7000,
+        date: new Date(2022, 4, 5)
+
+    },
+
+    {
+        id: "en3",
+        title: "Books",
+        amount: 540,
+        date: new Date(2022, 3, 22)
+
+    },
+
+    {
+        id: "en4",
+        title: "Kitchen Items",
+        amount: 950,
+        date: new Date(2022, 4, 8)
+
+    },
+
+];
+
+
 const App = () => {
 
-    let expenses = [
-        {
-            id: "en1",
-            title: "School Fee",
-            amount: 600,
-            date: new Date(2022, 4, 12)
+    const [expenses, setExpenses] =useState(Expense_data)
 
-        },
-
-        {
-            id: "en2",
-            title: "House Rent",
-            amount: 7000,
-            date: new Date(2022, 4, 5)
-
-        },
-
-        {
-            id: "en3",
-            title: "Books",
-            amount: 540,
-            date: new Date(2022, 3, 22)
-
-        },
-
-        {
-            id: "en4",
-            title: "Kitchen Items",
-            amount: 950,
-            date: new Date(2022, 4, 8)
-
-        },
-
-        {
-            id: "en5",
-            title: "Travel",
-            amount: 560,
-            date: new Date(2022, 4, 10)
-
-        }
-    ];
-
-    const dataFromChildToChild = (setDate) =>{
-        const data ={
-            ...setDate,
-            name:"simpy"
-        }
-        console.log(data);
+   
+    const dataFromChildToChild = (data) =>{
+       const updatedExenses = [data, ...expenses]
+       setExpenses (updatedExenses);
     }
 
     return (
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -4,33 +4,20 @@ import ExpenseItems from './ExpenseItems';
 import Cart from "../UI/Cart";
 
 const Expenses = (props) => {
+
     return (
+
+
         <Cart className='expenses '>
-            <ExpenseItems
-                date={props.item[0].date}
-                title={props.item[0].title}
-                amount={props.item[0].amount} />
-
-            <ExpenseItems
-                date={props.item[1].date}
-                title={props.item[1].title}
-                amount={props.item[1].amount} />
-
-            <ExpenseItems
-                date={props.item[2].date}
-                title={props.item[2].title}
-                amount={props.item[2].amount} />
-
-            <ExpenseItems
-                date={props.item[3].date}
-                title={props.item[3].title}
-                amount={props.item[3].amount} />
-
-<ExpenseItems
-                date={props.item[4].date}
-                title={props.item[4].title}
-                amount={props.item[4].amount} />
-
+            {
+                props.item.map((expense) => 
+                    (<ExpenseItems
+                        date={expense.date}
+                        title={expense.title}
+                        amount={expense.amount} />)
+                
+                )
+            }
 
         </Cart>
     );
